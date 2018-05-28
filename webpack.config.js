5652f6502ed18a89750e962d8fca3538e21b44ce
Administrator <admin@example.com>
2018-05-28 17:50:36 +0800

    antd跑起来了

--- webpack.config.js
+++ webpack.config.js
@@ -3,49 +3,60 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
+
 module.exports = {
-  mode:'development',
-  entry: path.join(__dirname, 'src', 'renderer','index'),
+  mode: 'development',
+  entry: ['babel-polyfill', path.join(__dirname, 'src', 'renderer', 'index')],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist','renderer')
+    path: path.resolve(__dirname, 'dist', 'renderer')
   },
   module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-    },{
-      test:/\.(jpg|png|gif|svg)$/,
-      exclude: /node_modules/,
-      loader:'file-loader'
-    },{
-      test:/\.(css)$/,
-      exclude: /node_modules/,     
-      use: [
-        {
-          loader:'style-loader'
-        },
-        {
-          loader:'css-loader',
-          options:{
-            modules:true       //开启css modules
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }, {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: 'file-loader'
+      }, {
+        test: /\.(css)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader',
+            options: {
+              modules: true       //开启css modules
+            }
           }
-        }
-      ]
-    }]
+        ]
+      }, {
+        test: /\.(css)$/,
+        include: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader',
+          }
+        ]
+      }
+    ]
   },
-  devServer:{
-    contentBase: path.join(__dirname,"dist"),
-    compress:true,
-    hot:true,
-    port:9000
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    hot: true,
+    port: 9000
   },
-  plugins:[
+  plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title:'sakura',
-      template:'./src/renderer/index.html'
+      title: 'sakura',
+      template: './src/renderer/index.html'
     }),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin()
